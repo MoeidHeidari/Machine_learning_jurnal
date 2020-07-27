0eaa8d774ee453a798539c00f1764531abf2a88f
moeidheidari <moeidheidari@hotmail.com>
2020-07-27 17:36:33 +0300

    Cmake added

---
+++ build/CMakeFiles/Machine_Learning_jurnal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Machine_Learning_jurnal.dir/main.cpp.o"
+  "Machine_Learning_jurnal"
+  "Machine_Learning_jurnal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Machine_Learning_jurnal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
